9cca2c1b325758fbd6e41eedc9c62b1e1c8faaec
AndrewKalil <1541@holbertonschool.com>
2020-06-14 16:58:00 -0500

    adding tasks 0 and 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,36 @@
 #include "sort.h"
 
 /**
- *
+ * selection_sort - sorts a list by choosing smallest int
+ * from the right to position on left
+ * @array: array of numbers
+ * @size: size of array
  */
+void selection_sort(int *array, size_t size)
+{
+        size_t i, j, tmp, min;
+
+        if (array == NULL || size <= 0)
+                return;
+        if (size > 1)
+        {
+                for (i = 0; i < (size - 1); i++)
+                {
+                        min = i;
+                        for (j = i + 1; j < size; j++)
+                        {
+                                if (array[j] < array[min])
+                                {
+                                        min = j;
+                                }
+                        }
+                        if (min != i)
+                        {
+                                tmp = array[min];
+                                array[min] = array[i];
+                                array[i] = tmp;
+                                print_array(array, size);
+                        }
+                }
+        }
+}
