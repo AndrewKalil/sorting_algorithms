f53beb50880c954a2ea80f847b2c0877a5590f84
AndrewKalil <1541@holbertonschool.com>
2020-06-15 15:12:11 -0500

    betty changes

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,29 +8,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-        size_t i, j, tmp, min;
+	size_t i, j, tmp, min;
 
-        if (array == NULL || size <= 0)
-                return;
-        if (size > 1)
-        {
-                for (i = 0; i < (size - 1); i++)
-                {
-                        min = i;
-                        for (j = i + 1; j < size; j++)
-                        {
-                                if (array[j] < array[min])
-                                {
-                                        min = j;
-                                }
-                        }
-                        if (min != i)
-                        {
-                                tmp = array[min];
-                                array[min] = array[i];
-                                array[i] = tmp;
-                                print_array(array, size);
-                        }
-                }
-        }
+	if (array == NULL || size <= 0)
+		return;
+	if (size > 1)
+	{
+		for (i = 0; i < (size - 1); i++)
+		{
+			min = i;
+			for (j = i + 1; j < size; j++)
+			{
+				if (array[j] < array[min])
+				{
+					min = j;
+				}
+			}
+			if (min != i)
+			{
+				tmp = array[min];
+				array[min] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
